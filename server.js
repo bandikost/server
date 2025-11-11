d78c1bb4c5d77c1849e80c43c8d309a13871b177
Roman <75956886+bandikost@users.noreply.github.com>
2025-11-11 15:15:06 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
+
+app.get("/api/blog", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM blog_posts ORDER BY id DESC");
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Ошибка сервера" });
+  }
+});
+
+app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));

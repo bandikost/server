1e8c51cf12c6b00989bc4eba9c5da942200f3034
Roman <75956886+bandikost@users.noreply.github.com>
2025-11-11 15:17:18 +0300

    Update server.js

--- server.js
+++ server.js
@@ -23,4 +23,6 @@ app.get("/api/blog", async (req, res) => {
   }
 });
 
-app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
